6c2b3d13e2960f8c52b0d96a3b8da54a6c7dc5aa
Rituj Manware <manwarerutuj@gmail.com>
2023-12-15 09:20:49 +0530

    added new router and controller

--- Backend/routers/coinapirouter.ts
+++ Backend/routers/coinapirouter.ts
@@ -1,11 +1,12 @@
 import express, { Router } from 'express';
-import { getAllTokens } from '../controller/coinController';
+import { getAllTokens, getCurrency } from '../controller/coinController';
 
 // Create a new router
 const coinRouter: Router = express.Router();
 
 // Define your routes
 coinRouter.get('/', getAllTokens);
+coinRouter.get('/currency',getCurrency)
 
 // Export the router
 export default coinRouter;
