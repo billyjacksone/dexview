d432983b80aaf0581f973a19ff800ed5c7a18f4e
sps03 <shantanusonwane20@cse.iiitp.ac.in>
2024-01-08 13:46:19 +0530

    Shivbar

--- app/NavigationBar/shibanavbar.tsx
+++ app/NavigationBar/shibanavbar.tsx
@@ -6,21 +6,7 @@ import Trending from '../components/Trendingpoly';
 import axios from 'axios';
 
 const sampleTrendingElements = [
-  // { rank: 1, name: 'JUGNI', Icon: () => <Image src="/polygon.png" width={20} height={20} alt="aoa" /> },
-  // { rank: 2, name: 'LK', Icon: () => <Image src="/polygon.png" width={20} height={20} alt="aoa" /> },
-  // { rank: 3, name: 'GFR', Icon: () => <Image src="/polygon.png" width={20} height={20} alt="aoa" /> },
-  // { rank: 4, name: 'DAI', Icon: () => <Image src="/polygon.png" width={20} height={20} alt="aoa" /> },
-  // { rank: 5, name: 'BTCETF', Icon: () => <Image src="/ethereum.webp" width={20} height={20} alt="aoa" /> },
-  // { rank: 6, name: 'BABYBONK', Icon: () => <Image src="/ethereum.webp" width={20} height={20} alt="aoa" /> },
-  // { rank: 7, name: 'SANTA', Icon: () => <Image src="/ethereum.webp" width={20} height={20} alt="aoa" /> },
-  // { rank: 8, name: 'GRAPE', Icon: () => <Image src="/ethereum.webp" width={20} height={20} alt="aoa" /> },
-  // { rank: 9, name: 'BABYGEMINI', Icon: () => <Image src="/ethereum.webp" width={20} height={20} alt="aoa" /> },
-  // { rank: 10, name: 'AGROK', Icon: () => <Image src="/ethereum.webp" width={20} height={20} alt="aoa" /> },
-  // { rank: 11, name: 'GEMINI', Icon: () => <Image src="/aoa.svg" width={20} height={20} alt="aoa" /> },
-  // { rank: 12, name: 'FTX', Icon: () => <Image src="/aoa.svg" width={20} height={20} alt="aoa" /> },
-  // { rank: 13, name: 'BABYCOQ', Icon: () => <Image src="/aoa.svg" width={20} height={20} alt="aoa" /> },
-  // { rank: 14, name: 'CXDO', Icon: () => <Image src="/aoa.svg" width={20} height={20} alt="aoa" /> },
-  // { rank: 15, name: 'AMPERE', Icon: () => <Image src="/aoa.svg" width={20} height={20} alt="aoa" /> },
+  { rank: 1, name: 'JUGNI', Icon: () => <Image src="/polygon.png" width={20} height={20} alt="aoa" /> },
   
 ];
 
