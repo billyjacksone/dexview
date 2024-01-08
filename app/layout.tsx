6657ca3dba3cd771ad7eabafca46bba3fe4ad0a4
sps03 <shantanusonwane20@cse.iiitp.ac.in>
2024-01-08 17:06:52 +0530

    layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,60 +1,3 @@
-// 'use client'
-// import React, { ReactNode, useEffect, useState } from 'react';
-// import './globals.css';
-// import { AppContextProvider } from '@/lib/contexts';
-// import NavigationHeader from './NavigationBar/Navigationheader';
-// import NavigationTrending from './NavigationBar/polynavtrending';
-// import NavigationTrending2 from './NavigationBar/Navigation';
-// import SideBar from './SideBar/SideBar';
-// import { Metadata } from 'next';
-// import { useRouter } from 'next/navigation';
-// import axios from 'axios';
-
-// interface LayoutProps {
-//   metadata: Metadata;
-//   children: ReactNode;
-// }
-
-// const RootLayout: React.FC<LayoutProps> = ({ metadata, children }) => {
-//   const [selectedItem, setSelectedItem] = useState<string | null>(null);
-//   const [tableData, setTableData] = useState<any[]>([]); // Add state for table data
-
-//   const router = useRouter();
-
-  
-  
-//   return (
-//     <html lang="en">
-//       <body>
-//         <AppContextProvider>
-//           <div style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh', position: 'relative'  }}>
-//            <div className='sticky top-0 z-10'>
-//             <NavigationHeader />
-//             <NavigationTrending />
-//            </div>
-
-//             <div style={{ display: 'flex', alignItems: 'flex-start', flex: '1' }}>
-//               <SideBar style={{ width: '300px', background: '#f0f0f0', padding: '20px', overflow: 'hidden' , paddingTop: '10px'}} />
-//               <div style={{ flex: '1', display: 'flex', flexDirection: 'column', paddingLeft: '6px', paddingTop: '4px', paddingBottom: '20px' }}>
-//                 {/* <TopBar /> */}
-//                 <div >
-                  
-//                   {selectedItem === null && children}
-//                 </div>
-//               </div>
-//             </div>
-
-//             <div style={{ borderTop: '1px solid #ddd', padding: '20px' }}>
-//               {/* Additional content */}
-//             </div>
-//           </div>
-//         </AppContextProvider>
-//       </body>
-//     </html>
-//   );
-// };
-
-// export default RootLayout;
 
 
 'use client'
@@ -62,6 +5,7 @@ import React, { Children, ReactNode, useEffect, useState } from 'react';
 import './globals.css';
 import { AppContextProvider } from '@/lib/contexts';
 import NavigationHeader from './NavigationBar/Navigationheader';
+
 import NavigationTrending from './NavigationBar/pulsenavtrending';
 import NavigationTrending2 from './NavigationBar/Navigation';
 import SideBar from './SideBar/SideBar';
@@ -70,6 +14,8 @@ import TopBar from './TopBar/Topbarelements';
 import { Metadata } from 'next';
 import { useRouter } from 'next/navigation';
 import axios from 'axios';
+
+
 
 interface LayoutProps {
   metadata: Metadata;
@@ -90,18 +36,10 @@ const RootLayout: React.FC<LayoutProps> = ({ metadata, children }) => {
         <AppContextProvider>
           <div style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh', position: 'relative'  }}>
            <div className='sticky top-0 z-10'>
-            <NavigationHeader />
+            <NavigationHeader isMetaMaskConnected={false} />
 
            </div>
 
-            {/* <div style={{ display: 'flex', alignItems: 'flex-start', flex: '1', marginTop: '0px' }}>
-              <div style={{ flex: '1', display: 'flex', flexDirection: 'column', paddingLeft: '0px', paddingTop: '0px', paddingBottom: '10px' }}>
-                <div style={{ flex: '1', overflow: 'hidden', paddingTop: '0px', paddingLeft: '0px' }}>
-                  
-                  {selectedItem === null && children}
-                </div>
-              </div>
-            </div> */}
             <div style={{display:'flex', alignItems: 'flex-start'}}>
               {children}
 
